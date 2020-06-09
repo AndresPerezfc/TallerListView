f0e0efadcea1c436528211fe59bc99d9e25f8a83
andresperezfc <felipe3apc@gmail.com>
2020-06-09 01:25:37 -0500

    Area del cuadrado terminada

--- app/src/main/java/com/example/tallerlistview/Cuadrado.java
+++ app/src/main/java/com/example/tallerlistview/Cuadrado.java
@@ -3,12 +3,53 @@ package com.example.tallerlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Cuadrado extends AppCompatActivity {
+
+    private EditText valor_lado;
+    private TextView resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuadrado);
+
+    valor_lado = findViewById(R.id.txtLado);
+    resultado = findViewById(R.id.txtResultado);
+    }
+
+    public void calcular(View v){
+        if(validar()) {
+            int lado, area;
+            lado = Integer.parseInt(valor_lado.getText().toString());
+            area = lado * lado;
+            resultado.setText("" + area);
+        }
+    }
+
+    public void limpiar(View v){
+        valor_lado.setText("");
+        resultado.setText("");
+        valor_lado.requestFocus();
+    }
+
+    public boolean validar(){
+        String error_valor_lado;
+
+        int op;
+
+        error_valor_lado = getResources().getString(R.string.error_valor_lado);
+
+        if(valor_lado.getText().toString().isEmpty()){
+            valor_lado.setError(error_valor_lado);
+            valor_lado.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
